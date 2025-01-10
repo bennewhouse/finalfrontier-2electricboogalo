6ad5bd538209c8fce0342f9576c61a4109ec9328
IlliterateJam <bennewhouse@vt.edu>
2025-01-10 12:26:40 -0500

    fixed tangling hair and rocky helmet interact #4219 by AlexOn1ine

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -9405,6 +9405,7 @@ BattleScript_GooeyActivates::
 	call BattleScript_AbilityPopUp
 	swapattackerwithtarget  @ for defiant, mirror armor
 	seteffectsecondary
+	swapattackerwithtarget
 	return
 
 BattleScript_AbilityStatusEffect::
